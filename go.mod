95a35d09425492477d270f7315979370888f4cba
Andy Cobaugh <andrew.cobaugh@gmail.com>
2021-01-12 19:35:06 -0500

    add healthcheck

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/PennState/go-ldappool
 go 1.15
 
 require (
+	github.com/PennState/go-healthcheck v0.0.0-20190813121746-d826779f2691
 	github.com/go-ldap/ldap/v3 v3.2.4
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
 	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
 	gopkg.in/ldap.v2 v2.5.1
 )
---
+++ healthcheck.go
@@ -0,0 +1,78 @@
+package ldappool
+
+import (
+	"fmt"
+	hc "github.com/PennState/go-healthcheck/pkg/health"
+	"github.com/go-ldap/ldap/v3"
+	"time"
+)
+
+// Healthcheck ...
+type Healthcheck struct {
+	Pool     Pool
+	SearchDN string // DN of LDAP entry to search for
+}
+
+const searchTimeLimit = 10
+
+// Check runs a gitlab client healthcheck
+func (c Healthcheck) Check() ([]hc.Check, hc.Status) {
+	searchCheck := hc.Check{
+		Key: hc.Key{
+			ComponentName:   "Search",
+			MeasurementName: "Result",
+		},
+		Output:        "",
+		Time:          time.Now().UTC(),
+		ComponentType: "component",
+		Links:         map[string]string{"dn": c.SearchDN},
+		Status:        hc.Pass,
+	}
+
+	poolSizeCheck := hc.Check{
+		Key: hc.Key{
+			ComponentName:   "Pool",
+			MeasurementName: "Size",
+		},
+		Output:        fmt.Sprintf("Pool size: %d", c.Pool.Len()),
+		Time:          time.Now().UTC(),
+		ComponentType: "component",
+		Status:        hc.Pass,
+	}
+
+	l, err := c.Pool.Get()
+	if err != nil {
+		searchCheck.Status = hc.Fail
+		searchCheck.Output = err.Error()
+
+		return []hc.Check{searchCheck, poolSizeCheck}, hc.Fail
+	}
+	defer l.Close()
+
+	res, err := l.Search(&ldap.SearchRequest{
+		BaseDN:     c.SearchDN,
+		Scope:      ldap.ScopeBaseObject,
+		Attributes: []string{},
+		SizeLimit:  1,
+		TimeLimit:  searchTimeLimit,
+		Filter:     "(objectclass=*)",
+	})
+	if err != nil {
+		searchCheck.Status = hc.Fail
+		searchCheck.Output = err.Error()
+
+		return []hc.Check{searchCheck, poolSizeCheck}, hc.Fail
+	}
+
+	fmt.Printf("results: %+v\n", res)
+
+	if len(res.Entries) != 1 {
+		searchCheck.Status = hc.Fail
+		searchCheck.Output = fmt.Sprintf("Search returned %d entries", len(res.Entries))
+
+		return []hc.Check{searchCheck, poolSizeCheck}, hc.Fail
+	}
+	searchCheck.Output = fmt.Sprintf("Found entry for %s", res.Entries[0].DN)
+
+	return []hc.Check{searchCheck, poolSizeCheck}, hc.Pass
+}
